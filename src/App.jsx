39bafa6ee5d06ceab2ab87b0af808b7d5eb24f8b
KingOfKings01 <aasifkhan9605@gmail.com>
2025-07-25 07:53:05 +0530

    try pushing project on git with login page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import SignIn from './pages/SignIn';
+import Login from './pages/Login'
 
 import Home from './pages/Home';
 // import Navbar from './components/Navbar';
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<>404 Page not found</>} />
       </Routes>
     </Router>
